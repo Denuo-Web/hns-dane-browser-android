c4471e5bf136cc1e2b651d16e1cbf08a8ef9f110
Jaron Rosenau <jaron.rosenau@gmail.com>
2026-06-27 02:31:54 -0700

    Add HNS Browser launcher assets

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.handshake.browser"
         minSdk = 34
         targetSdk = 37
-        versionCode = 4
-        versionName = "0.1.3"
+        versionCode = 5
+        versionName = "0.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
