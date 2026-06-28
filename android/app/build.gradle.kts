c7a84afce4505ce5a5a1d71760397e035c5a8e32
Jaron Rosenau <jaron.rosenau@gmail.com>
2026-06-28 03:41:42 -0700

    Bump version to 0.1.8

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.handshake.browser"
         minSdk = 34
         targetSdk = 37
-        versionCode = 8
-        versionName = "0.1.7"
+        versionCode = 9
+        versionName = "0.1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
